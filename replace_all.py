3ee010aba86057ae091e9829a0d2af4b4d033089
Mukesh chaudhary <mukeshchaudhary@Mukeshs-MacBook-Pro.local>
2019-11-11 16:03:50 -0600

    hit

--- replace_all.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-import numpy as np
-
-def replace_all(df,column_name):
-    df[column_name]=df[column_name].map(lambda x : x.replace(',',''))
-    df[column_name]=df[column_name].map(lambda x : x.replace('$',''))
-    return(df)
